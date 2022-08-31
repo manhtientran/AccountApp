1cd9ead177b16f1c863511d3829c3f1ce3abdeb5
Manh Tran <tienmanh.tran68@gmail.com>
2022-08-31 14:26:56 +0700

    Connect to database

--- core/Application.php
+++ core/Application.php
@@ -7,8 +7,9 @@ class Application {
     public Request $request;
     public Response $response;
     public static Application $app;
+    public Database $db;
 
-    public function __construct($rootPath) {
+    public function __construct($rootPath, array $config) {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
 
@@ -16,6 +17,7 @@ class Application {
         $this->response = new Response();
 
         $this->router = new Router($this->request,  $this->response);
+        $this->db = new Database($config["db"]);
 
     }
 
--- public/index.php
+++ public/index.php
@@ -4,7 +4,15 @@ require_once __DIR__."/../vendor/autoload.php";
 use app\controllers\AuthController;
 use app\core\Application;
 
-$app = new Application(dirname(__DIR__));
+$config = [
+    "db" => [
+        "dsn" => "mysql:host=127.0.0.1;port=3306;dbname=baa",
+        "user" => "root",
+        "password" => ""
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get("/", "home");
 
